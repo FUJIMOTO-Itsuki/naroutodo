b3a715f31e549b069e1852402f555f45ba41733a
FUJIMOTO,Itsuki <fujimoto.i.aa@m.titech.ac.jp>
2020-03-08 17:45:55 +0900

    アイテム追加可能に

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -7,6 +7,17 @@
         <div class="price">{{ item.price }} 円</div>
       </div>
     </div>
+    <div>
+      <label>
+        名前
+        <input type="text" v-model="newItemName" />
+      </label>
+      <label>
+        価格
+        <input type="number" v-model="newItemPrice" />
+      </label>
+      <button @click="addItem" v-bind:disabled="isNameEmpty">add</button>
+    </div>
   </div>
 </template>
 <script>
@@ -17,8 +28,22 @@ export default {
       items: [
         { name: "りんご", price: 100 },
         { name: "たまご", price: 160 }
-      ]
+      ],
+      newItemName: "",
+      newItemPrice: 0
     };
+  },
+  methods: {
+    addItem() {
+      this.items.push({ name: this.newItemName, price: this.newItemPrice });
+      this.newItemName = "";
+      this.newItemPrice = 0;
+    }
+  },
+  computed: {
+    isNameEmpty() {
+      return this.newItemName == "";
+    }
   }
 };
 </script>
