f3ee48624e8868a3d0842e9c3d338a0501ff7c8e
FUJIMOTO,Itsuki <fujimoto.i.aa@m.titech.ac.jp>
2020-03-09 00:32:05 +0900

    最低限のTodoリスト

--- src/components/todolist.vue
+++ src/components/todolist.vue
@@ -1,24 +1,31 @@
 <template>
   <div>
     <div>TodoList</div>
-    <div>ToDo</div>
-    <div v-for="item in items" :key="item.name">
-      <div class="item">
-        <div class="name" v-if="!item.done">名前: {{ item.name }}</div>
-      </div>
-    </div>
     <div>
-      <label>
-        名前
+      ToDo<label>
+        タスク名
         <input type="text" v-model="newItem.name" />
       </label>
 
-      <button @click="addItem" v-bind:disabled="isNameEmpty">add</button>
+      <button @click="addItem" v-bind:disabled="isNameEmpty">追加！</button>
+    </div>
+    <div v-for="(item, index) in items" :key="item.name">
+      <div class="item" v-if="!item.done">
+        <div class="name">
+          名前: {{ item.name }}
+          <button @click="switchDone(index)">完了した！</button>
+          <button @click="deleteItem(index)">削除</button>
+        </div>
+      </div>
     </div>
     <div>Done</div>
-    <div v-for="item in items" :key="item.name">
-      <div class="item">
-        <div class="name" v-if="item.done">名前: {{ item.name }}</div>
+    <div v-for="(item, index) in items" :key="item.name">
+      <div class="item" v-if="item.done">
+        <div class="name">
+          名前: {{ item.name }}
+          <button @click="switchDone(index)">実は未完…</button>
+          <button @click="deleteItem(index)">削除</button>
+        </div>
       </div>
     </div>
   </div>
@@ -39,6 +46,12 @@ export default {
     addItem() {
       this.items.push(this.newItem);
       this.newItem = { name: "", done: false };
+    },
+    switchDone(index) {
+      this.items[index].done = !this.items[index].done;
+    },
+    deleteItem(index) {
+      this.items.splice(index, 1);
     }
   },
   computed: {
